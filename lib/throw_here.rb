411322516f054eaecf83cb673ea2824782dddee9
abattalov <abattalov14@gmail.com>
2022-01-05 19:42:54 -0600

    add description

---
+++ lib/throw_here.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "throw_here/version"
+
+module ThrowHere
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/throw_here/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ThrowHere
+  VERSION = "0.1.0"
+end
