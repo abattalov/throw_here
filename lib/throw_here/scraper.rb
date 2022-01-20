a6c2c9e97617c55d40bf116fda6b391ee1b58af2
Alisher <abattalov14@gmail.com>
2022-01-19 23:25:43 -0600

    cleaned up scraper code

--- lib/throw_here/scraper.rb
+++ lib/throw_here/scraper.rb
@@ -27,7 +27,7 @@ class ThrowHere::Scraper
         while @page <= @final_page
             pagination_doc = Nokogiri::HTML(open("https://www.pdga.com/course-directory/advanced?title=&field_course_location_country=US&field_course_location_locality=&field_course_location_administrative_area=All&field_course_location_postal_code=&field_course_type_value=All&rating_value=All&field_course_holes_value=All&field_course_total_length_value=All&field_course_target_type_value=All&field_course_tee_type_value=All&field_location_type_value=All&field_course_camping_value=All&field_course_facilities_value=All&field_course_fees_value=All&field_course_handicap_value=All&field_course_private_value=All&field_course_signage_value=All&field_cart_friendly_value=All&page=#{@page}"))
                         
-            tbody_contents = pagination_doc.css('tbody')[0]
+            tbody_contents = pagination_doc.css('tbody')
     
             @name = tbody_contents.css('.views-field.views-field-title')
             @city = tbody_contents.css('.views-field.views-field-field-course-location')
@@ -46,7 +46,7 @@ class ThrowHere::Scraper
     end
 
     def get_course_details
-        @name.each do |listing|
+        @name.each do
         course = ThrowHere::Course.new
         course.name = @name[@counter].css('a').text.strip
         course.city = @city[@counter].text.strip
